1cbfdb05e595ab24adffc8db8b6e16da0d4b5b42
RotondwANetsh <87181553+RotondwANetsh@users.noreply.github.com>
2022-01-03 09:42:00 +0200

    First commit

---
+++ src/ArraysChallenge.java
@@ -0,0 +1,20 @@
+public class ArraysChallenge {
+    public static void print(int i){
+        if (i % 3 == 0 && i % 5 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if (i % 3 ==0){
+            System.out.println("Fizz");
+        }
+        else if (i % 5 == 0){
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        print(101);
+    }
+}
