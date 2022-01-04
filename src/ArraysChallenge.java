08b9a0d472d0af50b5a6733810441c11c8d0e343
Rotondwa Netshimbupfe <87181553+RotondwANetsh@users.noreply.github.com>
2022-01-04 10:03:08 +0200

    Add files via upload

--- src/ArraysChallenge.java
+++ src/ArraysChallenge.java
@@ -1,20 +1,30 @@
 public class ArraysChallenge {
-    public static void print(int i){
-        if (i % 3 == 0 && i % 5 == 0){
-            System.out.println("FizzBuzz");
-        }
-        else if (i % 3 ==0){
-            System.out.println("Fizz");
-        }
-        else if (i % 5 == 0){
-            System.out.println("Buzz");
-        }
-        else {
-            System.out.println(i);
-        }
-    }
-
     public static void main(String[] args) {
-        print(101);
+        int[] array = new int[50];
+        int i = 0,  j = 0;
+
+        System.out.println("\nOriginal array:");
+        while (i < array.length){
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i] + " ");
+            i++;
+        }
+        System.out.println("\n\nEven numbers and divisible by 11 only values");
+        while (j < array.length) {
+
+            int even = array[j] % 2;
+            int divisible = array[j] % 11;
+
+            switch (even){
+                case 0:
+                    System.out.print(array[j] + " ");
+                    break;
+            }
+            switch (divisible){
+                case 0:
+                    System.out.print(array[j] + " ");
+                    break;
+            }
+            j++;
+        }
     }
-}
---
+++ src/ArraysChallenge5.java
@@ -0,0 +1,14 @@
+public class ArraysChallenge {
+   public static void main(String[] args) {
+       int[] array = new int[50];
+
+       for (int i = 0; i < array.length; i++) {
+           if(i % 2 == 0){
+               System.out.print(i + " ");
+           }
+           else if(i % 11 == 0){
+               System.out.print(i+ " ");
+           }
+       }
+   }
+}
